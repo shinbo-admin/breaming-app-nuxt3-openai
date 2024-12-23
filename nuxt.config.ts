36fc39d67cfe3d2209dc366a4188ce249ad74b78
ShinsukeMaeda <sh.maeda@aiqveone.co.jp>
2024-12-23 14:10:28 +0900

    add vuetifyを導入

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,15 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  css: ['@/assets/main.scss', '@mdi/font/css/materialdesignicons.min.css'],
+  css: ['@/assets/main.scss', '@mdi/font/css/materialdesignicons.min.css', 'vuetify/styles'],
+  build: {
+    transpile: ['vuetify'],
+  },
+  vite: {
+    define: {
+      'process.env.DEBUG': false,
+    },
+  },
   app: {
     head: {
       meta: [
